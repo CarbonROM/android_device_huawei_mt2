dbe58c78a707de7b189a3a94dc669cc9e593cff6
PhaseBurn <phaseburn@phaseburn.net>
2015-09-09 20:42:40 -0700

    Initial clone

---
+++ carbon.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+$(call inherit-product, device/huawei/mt2/full_mt2.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/mt2/mt2.mk)
+
+PRODUCT_DEVICE := mt2
+PRODUCT_NAME := carbon_mt2
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=MT2L03 \
+	BUILD_FINGERPRINT=Huawei/MT2L03/hwMT2L03:5.1/HuaweiMT2L03/C00B320:user/release-keys \
+	PRIVATE_BUILD_DESC="MT2-L03-user 5.1 GRJ90 C00B320 release-keys"
